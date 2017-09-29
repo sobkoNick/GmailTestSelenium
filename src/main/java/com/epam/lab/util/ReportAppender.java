1a98bf71c86fd495b9e379224a767a4d4d2af45d
DESKTOP-6SSJG4S\Mykola <sobko.nick@gmail.com>
2017-09-29 12:12:56 +0300

    OutPut log added for html page.

---
+++ src/main/java/com/epam/lab/util/ReportAppender.java
@@ -0,0 +1,39 @@
+package com.epam.lab.util;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Layout;
+import org.apache.log4j.spi.LoggingEvent;
+import org.testng.Reporter;
+
+/**
+ *
+ */
+public class ReportAppender extends AppenderSkeleton {
+    @Override
+    protected void append(LoggingEvent loggingEvent) {
+        Reporter.log(eventToString(loggingEvent));
+    }
+
+    private String eventToString(final LoggingEvent event) {
+        final StringBuilder result = new StringBuilder(layout.format(event));
+        if (layout.ignoresThrowable()) {
+            final String[] s = event.getThrowableStrRep();
+            if (s != null) {
+                for (final String value : s) {
+                    result.append(value).append(Layout.LINE_SEP);
+                }
+            }
+        }
+        return result.toString(); // add a line break since the output is html format
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public boolean requiresLayout() {
+        return true;
+    }
+}
